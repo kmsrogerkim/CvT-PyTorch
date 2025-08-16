d40d605ec7c59ff9c7f2aa3cf2909a038fc88291
roger <minseungkim1017@gmail.com>
2025-08-16 17:46:51 +0900

    feat: init

---
+++ README.md
@@ -0,0 +1,28 @@
+# YOLO (You Only Look Once) Model
+
+YOLO is a state-of-the-art, real-time object detection system. Its name emphasizes that the algorithm makes predictions for objects after looking at the image only once.
+
+The model is known for its speed and accuracy, making it a popular choice for real-time applications.
+
+## Model Architecture
+
+Here is a visualization of the YOLO model architecture.
+
+![YOLO Model](./media/model.png)
+
+_Figure 1. YOLO model architecture_ [1]
+
+## Data
+**Credit to [Aladdin Persson](https://github.com/aladdinpersson/Machine-Learning-Collection/tree/master/ML/Pytorch/object_detection/YOLO)**
+https://www.kaggle.com/datasets/734b7bcb7ef13a045cbdd007a3c19874c2586ed0b02b4afc86126e89d00af8d2
+
+## Model Loss
+
+The following graph shows the training loss over epochs, which helps in evaluating the model's performance.
+
+![Model Loss](./media/loss.png)
+
+_Figure 2. Loss function for YOLO_ [1]
+
+## References
+1. Redmon, Joseph, et al. "You only look once: Unified, real-time object detection." _Proceedings of the IEEE conference on computer vision and pattern recognition. 2016._
---
+++ model.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+
+class ConvTokenEmbedding(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class ConvTransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.depth_wise_seperable_conv = self.make_depth_wise_seperable_conv()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return
+
+    def make_depth_wise_seperable_conv(self, in_ch, out_ch, k, s):
+        return nn.Sequential(
+            # depth wise
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch,
+                      kernel_size=k, padding=k//2, stride=s,
+                      groups=in_ch),
+            nn.BatchNorm2d(in_ch),
+            nn.GELU(),
+
+            # point wise
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch,
+                      kernel_size=1),
+            nn.BatchNorm2d(out_ch),
+        )
+
+class CvT(nn.Module):
+    def __init__(self, init_weight=True):
+        super().__init__()
+        if init_weight:
+            self._initialize_weights()
+
+        self.darknet = self._make_conv_layers()
+        self.fully_connected_layer = self._make_fc_layers()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.darknet(x)
+        x = self.fully_connected_layer(x)
+        return x
+
+    def _make_conv_layers(self):
+        darknet = nn.Sequential(
+            nn.Conv2d(3, 64, 7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 192, 3, padding=1),
+            nn.BatchNorm2d(192),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(192, 128, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 256, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(512, 256, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 256, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 512, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(1024, 512, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 512, 1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, 3, stride=2, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+
+            nn.Conv2d(1024, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(1024, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.1, inplace=True)
+        )
+        return darknet 
+
+    def _make_fc_layers(self):
+        fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(S*S*1024, 4096),
+            nn.LeakyReLU(0.1),
+            nn.Linear(4096, S*S*(B*5+C))
+        )
+        return fc    
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
